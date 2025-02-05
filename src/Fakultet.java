08e6213132c7940477b1337237cb777bd39b141e
pthormodsen <patrik.mikal.thormodsen@gmail.com>
2025-02-05 10:04:15 +0100

    test

--- src/Fakultet.java
+++ src/Fakultet.java
@@ -1,6 +1,8 @@
 public class Fakultet {
     public static void main(String[] args) {
         System.out.println("5! = " + fakultet(5));
+
+        System.out.println("6! = " + fakultet(6));
     }
 
     public static int fakultet(int n) {
